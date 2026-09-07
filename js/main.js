b14f55e2e1cc8b3b9a66155c43249a9c7154f2af
Yogachandrasekar <yogachandrasekar223@gmail.com>
2026-09-07 09:20:39 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+            $('.nav-bar').addClass('fixed-top').css({'margin': '0', 'border-radius': '0'});
+        } else {
+            if ($(window).width() > 992) {
+                $('.nav-bar').removeClass('fixed-top').css({'margin': '0px 90px', 'border-radius': '50px'});
+            } else {
+                $('.nav-bar').removeClass('fixed-top').css({'margin': '0px 30px', 'border-radius': '50px'});
+            }
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+})(jQuery);
+
